1c869a0134657d0cf9a5103fa1fe6cb9bde621f1
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-09-02 01:24:25 -0400

    halt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,14 +19,14 @@ class CPU:
         self.fl: int = 0 # flags bitfield 00000LGE
 
         self.commands = {
-            0x00: self.no_op, # NOP  0
-            0x01: self.halt, # HLT  0
-            0x11: self.return_from_subroutine, # RET  0
-            0x13: self.return_from_interrupt_handler, # IRET 0
-            0x45: self.push, # PUSH 1
-            0x46: self.pop, # POP  1
-            0x47: self.print_number, # PRN  1
-            0x48, # PRA  1
+            0x00: self.no_op,
+            0x01: self.halt, # !!!!!!!!
+            0x11: self.return_from_subroutine,
+            0x13: self.return_from_interrupt_handler,
+            0x45: self.push,
+            0x46: self.pop,
+            0x47: self.print_number,
+            0x48: self.print_alpha,
             0x50, # CALL 1
             0x52, # INT  1
             0x54, # JMP  1
@@ -39,27 +39,51 @@ class CPU:
             0x65, # INC  1
             0x66, # DEC  1
             0x69, # NOT  1
-            0x82, # LDI  2
+            0x82, # LDI  2 !!!!!!!
             0x83, # LD   2
             0x84, # ST   2
-            0xA0, # ADD  2
-            0xA1, # SUB  2
-            0xA2, # MUL  2
-            0xA3, # DIV  2
-            0xA4, # MOD  2
-            0xA7, # CMP  2
-            0xA8, # AND  1
-            0xAA, # OR   2
-            0xAB, # XOR  2
-            0xAC, # SHL  2
-            0xAD, # SHR  2
+            0xA0: self.add,
+            0xA1: self.sub,
+            0xA2: self.mul,
+            0xA3: self.div,
+            0xA4: self.mod,
+            0xA7: self.compare,
+            0xA8: self.bit_and,
+            0xAA: self.bit_or,
+            0xAB: self.bit_xor,
+            0xAC: self.bit_shift_left,
+            0xAD: self.bit_shift_right,
         }
+
+        self.alu_ops = {
+            'ADD': lambda reg_a, reg_b: self.reg[reg_a] += self.reg[reb_b],
+            'SUB': lambda reg_a, reg_b: self.reg[reg_a] -= self.reg[reb_b],
+            'MUL': lambda reg_a, reg_b: self.reg[reg_a] *= self.reg[reb_b],
+            'DIV': lambda reg_a, reg_b: self.reg[reg_a] /= self.reg[reb_b],
+            'MOD': lambda reg_a, reg_b: self.reg[reg_a] %= self.reg[reb_b],
+            # 'CMP': lambda reg_a, reg_b: self.reg[reg_a] += self.reg[reb_b],
+            'AND': lambda reg_a, reg_b: self.reg[reg_a] &= self.reg[reb_b],
+            'OR': lambda reg_a, reg_b: self.reg[reg_a] |= self.reg[reb_b],
+            'XOR': lambda reg_a, reg_b: self.reg[reg_a] ^= self.reg[reb_b],
+            'SHL': lambda reg_a, reg_b: self.reg[reg_a] <<= self.reg[reb_b],
+            'SHR': lambda reg_a, reg_b: self.reg[reg_a] >>= self.reg[reb_b],
+        }
+
+    class HaltExcpetion(Exception):
+        __init__(self, message: str):
+            super().__init__(message)
+
+    def get_one(self) -> int:
+        pass
+
+    def get_two(self) -> (int, int):
+        pass
 
     def no_op(self):
         pass
 
     def halt(self):
-        pass
+        raise HaltExcpetion('halted')
 
     def return_from_subroutine(self):
         pass
@@ -68,13 +92,64 @@ class CPU:
         pass
 
     def push(self):
+        reg_a = self.get_one()
         pass
 
     def pop(self):
+        reg_a = self.get_one()
         pass
 
     def print_number(self, register: int):
+        reg_a = self.get_one()
+        print(self.reg[reg_a])
+
+    def print_alpha(self, register: int):
+        reg_a = self.get_one()
+        print(chr(self.reg[reg_a]))
+
+    def add(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def sub(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def mul(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def div(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def mod(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def compare(self):
+        reg_a, reg_b = self.get_two()
         pass
+
+    def bit_and(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def bit_or(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def bit_xor(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def bit_shift_left(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
+
+    def bit_shift_right(self):
+        reg_a, reg_b = self.get_two()
+        self.alu('ADD', reg_a, reg_b)
 
     def load(self):
         """Load a program into memory."""
@@ -105,11 +180,9 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
+        try:
+            self.alu_ops[op]()
+        except KeyError
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
@@ -132,7 +205,7 @@ class CPU:
 
         print()
 
-    def set_next_state(self) -> bool:
+    def run_next_command(self) -> bool:
         self.mar += 1
         self.pc = self.mar
         try:
@@ -140,8 +213,14 @@ class CPU:
             self.commands[self.ir]()
         except IndexError:
             return False
+        except KeyError:
+            return False
+        return True
 
     def run(self):
         """Run the CPU."""
-        while self.set_next_state():
+        try:
+            while self.run_next_command():
+                pass
+        except HaltExcpetion:
             pass
